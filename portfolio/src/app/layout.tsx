af65dc52cf41208a3a8f10697df6a4380a0e24cc
nh602 <nhenley602@gmail.com>
2025-03-13 13:24:44 -0600

    Changed background image. Updated card bg color. Removed sticky header background. General changes to main page with mobile testing

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins} antialiased w-screen h-screen overflow-clip`}>
-        <div className={"absolute inset-0 bg-slate-100 w-0 h- border-b-[75vw] border-[#666F75] border-l-[100vw] border-l-transparent"}></div>
-        <div className={"absolute inset-0 w-screen h-screen bg-no-repeat bg-center bg-cover bg-left-bottom bg-fixed"} style={{ backgroundImage: "url('bg-strokes.svg')"}}>
+      <body className={`${poppins} antialiased w-screen h-screen overflow-clip bg-slate-100`}>
+        <div className={"absolute inset-0 w-screen h-screen bg-no-repeat bg-cover bg-bottom bg-fixed"} style={{ backgroundImage: "url('bg5.png')"}}>
+        {/* <div className={"absolute inset-0 bg-transparent w-0 h- border-b-[100vw] border-[#666f75] border-l-[115vw] border-l-transparent"}></div> */}
           {/* <div className="absolute bottom-0 translate-y-[100] w-1/2 h-1/2 rounded-t-full rotate-45 border-l-[5px] border-l-gray-200"></div>
           <div className="absolute top-0 -translate-y-[10vh] left-0 w-1/2 h-1/2 rounded-b-full -rotate-90 border-l-[5px] border-l-gray-200"></div>
           <div className="absolute -translate-y-[10vh] translate-x-[25vw] w-1/2 h-1/2 -rotate-[20deg] rounded-t-full border-l-[5px] border-l-gray-200"></div> */}
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -26,32 +26,45 @@ export default function Home() {
     <>
       <div className="flex flex-col h-screen overflow-y-scroll gap-0 snap-y snap-mandatory no-scrollbar">
         <Section className="relative overflow-x-clip">
-          <div className="w-screen h-screen flex flex-col justify-center items-center">
-            <div className="p-10 shadow-lg flex flex-row gap-60 justify-center items-center bg-slate-100 border-t-2 border-black">
-              <div className="flex flex-col gap-5 p-10 h-min">
-                <div className="flex flex-col gap-2">
+          <div className="w-screen h-screen flex flex-col justify-center items-center p-4 sm:p-0">
+            <div className="p-4 sm:p-6 shadow-lg flex flex-col sm:flex-row w-full sm:w-2/3 overflow-x-clip overflow-y-clip sm:justify-center items-center bg-[#E8ECEF] bg-opacity-65 border-t-2 border-black">
+              <div className="flex flex-col basis-1/2 gap-2 sm:gap-10 p-2 sm:p-10 h-min">
+                <div className="flex flex-col gap-2 items-center">
                   <h1 className="text-6xl text-center font-bold">Nick Henley</h1>
                   <h2 className="text-2xl text-center font-bold text-gray-500">Software Developer</h2>
+                  <div className="flex gap-3 w-full sm:w-1/2 items-center justify-center h-min items-stretch border-t-[1px] border-b-[1px] sm:border-b-[0px] border-black py-2 sm:pt-5">
+                    <a target="_blank" href="mailto:nicholas.henley@colorado.edu" className="flex items-center text-indigo-400 pr-3 border-r-2 border-black">
+                      <img src="/icons/mail.png" alt="linkedin" className="w-10" />
+                    </a>
+                    <a target="_blank" href="https://linkedin.com/in/nhenley" className="flex items-center pr-3 border-r-2 border-black">
+                      <img src="/icons/linkedin.png" alt="linkedin" className="w-10" />
+                    </a>
+                    <a target="_blank" href="https://github.com/nh602" className="flex items-center pr-3">
+                      <img src="/icons/github.png" alt="github" className="w-10" />
+                    </a>
+                  </div>
                 </div>
-                <div className="flex gap-3 w-full items-center justify-center h-min items-stretch border-t-[1px] border-black pt-5">
-                  <a target="_blank" href="mailto:nicholas.henley@colorado.edu" className="flex items-center text-indigo-400 pr-3 border-r-2 border-black"><img src="/icons/mail.png" alt="linkedin" className="w-10" /></a>
-                  <a target="_blank" href="https://linkedin.com/in/nhenley" className="flex items-center pr-3 border-r-2 border-black">
-                    <img src="/icons/linkedin.png" alt="linkedin" className="w-10" />
-                  </a>
-                  <a target="_blank" href="https://github.com/nh602" className="flex items-center pr-3">
-                    <img src="/icons/github.png" alt="github" className="w-10" />
-                  </a>
+                <div>
+                  <p className="text-wrap text-center sm:text-left">
+                    I’m a programmer with a deep interest in cybersecurity, machine learning, and embedded systems. 
+                    My approach is hands-on and practical — I like to understand how 
+                    things work at a fundamental level and apply that knowledge to build better, more secure systems. 
+                    I enjoy tackling real-world challenges in security and AI, and I’m eager to collaborate on projects that push the boundaries of 
+                    what’s possible in these fields. If you’re working on something exciting in cybersecurity, AI, or embedded systems, let’s connect!
+                  </p>
                 </div>
               </div>
-              <div className="overflow-clip shadow-2xl border-slate-300 border-[1px] bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-sky-100 to-slate-50 p-5 rounded-full h-[450px] w-[450px]">
-                <img src="headshot.png" className=""/>
+              <div className="collapse sm:visible">
+                <div className="overflow-clip shadow-lg p-5 rounded-full h-[350px] w-[350px]">
+                  <img src="headshot.png" className=""/>
+                </div>
               </div>
             </div>
           </div>
         </Section>
         {/* Sticky header after first section */}
-        <div className="sticky top-0 flex flex-row w-screen justify-between items-center pl-10 pr-20">
-            <div className="flex flex-col items-start bg-slate-100 w-min">
+        <div className="collapse md:visible sticky top-0 flex flex-row w-screen justify-between items-center pl-10 pr-20 py-2">
+            <div className="flex flex-col items-start w-min">
               <h1 className="text-right text-4xl font-bold text-gray-800 text-nowrap">Nick Henley</h1>
               <h2 className="text-right text-1xl text-gray-500">Software Developer</h2>
               <p className="text-right">Broomfield, CO</p>
@@ -71,7 +84,6 @@ export default function Home() {
         <Section>
           { selectedProject && <ProjectModal project={selectedProject} closeModal={closeProjectModal}/> }
           <div className="w-screen h-screen flex flex-col gap-5 md:gap-10 pt-5 justify-center items-center">
-            <h1 className="text-center text-4xl md:text-6xl font-bold text-gray-800 text-nowrap z-10">Projects</h1>
             <CardCarousel>
               <Card project={homelab} onClick={setSelectedProject}/>
               <Card project={portfolioProject} onClick={setSelectedProject}/>
